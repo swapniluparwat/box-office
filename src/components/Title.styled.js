9e1fc6ebba5dbc74d74e2e3d7e331d4ec568dde3
Swapnil <smuparwat@gmail.com>
2021-12-27 00:52:34 +0530

    defined theme for styled components and create few styled components

---
+++ src/components/Title.styled.js
@@ -0,0 +1,16 @@
+export const TitleWrapper = styled.div`
+  text-align: center;
+  margin: 0 0 40px;
+
+  h1 {
+    color: ${({ theme }) => theme.mainColors.blue};
+    letter-spacing: 10px;
+    text-transform: uppercase;
+    margin: 0 0 10px;
+  }
+
+  p {
+    color: ${({ theme }) => theme.mainColors.dark};
+    margin: 0;
+  }
+`;
